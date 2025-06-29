092be7f6d51f538049fd009fe6680361d46235ec
faggy001 <faggy29@gmail.com>
2025-06-29 09:17:52 +0100

    Backend.tf modified

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 # remote backend
 terraform {
   backend "s3" {
-    bucket         = "groupb-terraform-007"
+    bucket         = "groupb-terraform-009"
     key            = "tfstate-serverless/terraform.tfstate"
     region         = "ca-central-1"
     encrypt        = true
